62bc4168149814bc331289e3d28bf2fbd1af0fbf
Ariba-memon <aribaabdulqadir786@gmail.com>
2024-03-20 21:05:14 +0300

    today's code

---
+++ Class03/app.js
@@ -0,0 +1,187 @@
+// //increment decrement
+
+// Post Increment // Post Decrement
+// var num = 5
+// // num + 1 = 6
+//  num++ ; //6  num--
+//  console.log(num) //6
+
+
+// var num_Three = 4;
+// num_Three  + 1
+// num_Three++;
+
+// console.log(num_Three) //5
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ //Pre Increment  Pre Decrement
+
+//  var numTwo = 6;
+//  // 1 + numTwo
+//  ++numTwo; --numTwo
+//  console.log(numTwo) //7
+
+
+ 
+
+
+// var x = 5;
+// var y= 6;
+// var z = x++  + ++y;
+//          5  + 7 //12    
+// console.log(z)
+
+
+// var value =5 
+// value++ //6
+
+// var x = 5;
+// var y= 6;
+// var z = x++ +x  + ++y;
+//           5 + 6 + 7
+// console.log(z)
+
+
+
+
+
+
+
+
+
+
+
+// //BODMAS RULE
+
+
+
+
+
+// //ASSIGNMENT OPERATOR
+// //KISI BHI VARIABLE MAIN KOI VALUE ASSIGN KRRHY HAIN
+
+// var age =2
+// //age + 4 = 19
+// // age +=4;
+// // age -=4;
+// // age *=4
+// age **=4
+// // 2*2*2*2
+// console.log(age) //
+
+
+//prompt 
+// var person = prompt('What is Your Name');
+// console.log(person)
+ 
+
+
+
+
+
+
+
+
+
+
+// var age =20
+// age -=5;
+// //age = age -5  //15
+// console.log(age)
+
+
+//age += 2 //
+//age -= 2
+//age *=2 //
+//
+
+
+// var age = 2;
+// age **=5;
+// //2*2*2*2*2
+// console.log(age)
+
+
+
+
+// //JavaScript Comparison Operators
+
+// // < less than
+// // > greater than
+// //== double equal kiaa yeh donovalue barabar hai
+// //=== triple equals to
+// // != not equals to yeh value isky barabar nh hai
+// //>= greater than equals to yeh number ya tu is number sy bara hai ya isky barabar hai
+// //<= less than equals to yeh number ya tu is number sy chota ho ya isky barabar ho
+
+
+// var value =2 <3;
+// console.log(value)
+
+// var value =2 >3;
+// console.log(value)
+
+
+
+
+
+
+// var value = 3==4;
+// console.log(value)
+
+
+
+
+
+// var value = 3!=4;
+// console.log(value)
+
+
+
+
+
+
+
+
+
+// var value = 3>=3;
+// console.log(value)
+
+
+// var value = 4<=3;
+// console.log(value)
+
+
+
+
+
+
+// var value = 2 === 2
+// console.log(value)
+
+
+
+
+
+
+
+
+
+// var value = 3=='3';
+// console.log(value) //value main number konsa hai
+
+// //triple equal to type bhi check krta hai and number bhi
+
+// var value = '3'==='3';
+// console.log(value)
+
+
