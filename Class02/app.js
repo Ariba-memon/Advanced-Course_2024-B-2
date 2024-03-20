3673d34c8a9626ae887d9def8985c348793e4f88
Ariba-memon <aribaabdulqadir786@gmail.com>
2024-03-20 06:20:26 +0300

    first commit

---
+++ Class02/app.js
@@ -0,0 +1,57 @@
+//javascript brain hai kisi bhi website ka
+
+//ecmascript es5 //2015 // 10 days
+
+// alert('Hello world')
+
+//syntax 
+//wait  
+//var  
+//variable
+
+//= assning operator
+//a 
+// var a = "Hello world" // string 
+// console.log(a)
+
+//function console.log 
+//errors ko dekhny ky liye console krwaya jata h
+//debugging
+
+//"helloworld" , 'helloworld' //string
+
+// var num = 1 ;
+// console.log(num)
+
+// 1 ,022220, 0.7 0.5 
+
+//concatenation
+// var firstName = "Ariba"
+// var secondName = " Memon"
+// console.log("My Name is " + firstName + secondName)
+
+//camelcase
+
+
+// operators  //+ - * / %
+// var a = 15;
+// var b = 7
+// console.log(a % b)  // 30  //20 //8  //1
+
+// boolean
+
+// var name = "Ayesha"
+// var age = 80
+// var ayesha_alive = true //false
+
+// if and else 
+// var a = 9
+// if (a > 10){
+// console.log('Big Value')
+// }
+// else{
+// console.log('small value')
+// }
+
+
+
