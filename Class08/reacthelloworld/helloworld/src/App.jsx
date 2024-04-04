0848ecd1992b8db7ab56c82ecc21b77e399079c6
Ariba-memon <aribaabdulqadir786@gmail.com>
2024-04-04 23:12:46 +0300

    reactbasics

---
+++ Class08/reacthelloworld/helloworld/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <div>
+<h1 >hello world</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, mollitia! Sed incidunt debitis laborum commodi repudiandae quibusdam delectus, inventore pariatur suscipit distinctio nam ad iusto? Reiciendis suscipit ratione est quia!</p>
+   </div>
+  );
+}
+
+export default App;
