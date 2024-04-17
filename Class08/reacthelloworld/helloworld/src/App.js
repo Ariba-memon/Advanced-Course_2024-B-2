6af63662e64eb194810f9bec9ec852461a6a66e1
Ariba-memon <aribaabdulqadir786@gmail.com>
2024-04-17 20:30:51 +0500

    class09

---
+++ Class08/reacthelloworld/helloworld/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import { useState } from 'react';
+function App() {
+  let [Counter, setCounter] = useState(0);
+  //arrow function
+  const Inc = () =>{
+  setCounter(Counter + 1)
+  }
+  //Decrement
+  const Dec = () =>{
+    setCounter(Counter - 1)
+    }
+  return (
+   <div>
+  <h1>Counter App </h1>
+  <h1>{Counter}</h1>
+  <button onClick={Inc}>Increment</button>
+  <button onClick={Dec}>Decrement</button>
+   </div>
+  );
+}
+
+export default App;
